685f757ea258019ab4a1c25ca02a3ec08c1e4c21
AtulDeshpande09 <deshpandeatul05@gmail.com>
2025-07-07 23:19:19 +0530

    spacy for sentence analysis - enhanced modularity

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from rules import tokenize , to_mr
+from rules import to_mr
 
 def translate():
 
     print("English → Marathi (Rule-based FST Translator)")
 
     sentence1 = "She eats apple."
-    tok1 = tokenize(sentence1)
-    mr_1 = to_mr(tok1)
+    mr_1 = to_mr(sentence1)
     print(sentence1)
     print(mr_1)
 
     sentence2 = "He drinks water."
-    tok2 = tokenize(sentence2)
-    mr_2 = to_mr(tok2)
+    mr_2 = to_mr(sentence2)
     print(sentence2)
     print(mr_2)
 
--- rules.py
+++ rules.py
@@ -1,33 +1,60 @@
 from lexicon import lexicon
 from morph import inflect_verb
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def detect_tense_spacy(doc):
+    for token in doc:
+        if token.pos_ == "VERB":
+            if token.tag_ in ["VBD", "VBN"]:
+                return "past"  # Past
+            elif token.tag_ in ["VBZ", "VBP", "VB"]:
+                return "present"  # Present
+            elif token.tag_ == "MD":
+                return "future"  # Future
+    return 0  # Default to present
+
+def analyze_sentence(sentence:list):
+    """
+    Input : sentence (str)
+    Return :
+    dict containing - tense , subj , obj , verb .
+    """
+    sentence = sentence.lower()
+    doc = nlp(sentence)
+    tense = detect_tense_spacy(doc)
+    for token in doc:
+        if token.dep_ == "nsubj":
+            subj = token.text
+        elif token.dep_ == "dobj":
+            obj = token.text
+        elif token.pos_ == "VERB":
+            verb = token.lemma_
+    return { "tense" : tense , "subj" : subj , "verb" : verb , "obj" : obj }
 
 
-def tokenize(sentence):
-    sentence = sentence.strip('.?!').lower()
-    return sentence.split()
+def to_mr(sentence):
 
+    sent_dict = analyze_sentence(sentence)
 
-def to_mr(tokens):
-
-    sub_en , verb_en , obj_en = tokens[0] , tokens[1][:-1] , tokens[2]
+    sub_en = sent_dict["subj"]
+    obj_en = sent_dict["obj"]
+    verb_en = sent_dict["verb"]
+    tense = sent_dict["tense"]
     
-    #print(verb_en)
-
     sub = lexicon.get(sub_en)
     verb = lexicon.get(verb_en)
     obj = lexicon.get(obj_en)
-
 
     sub_mr = sub["mr"]
     plural = sub["number"]
     person = sub["person"]
     
-    obj_mr = obj['mr']
-
-    verb_rt = verb['root']
     gender = sub['gender']
-    tense = verb["tense"]
-
+    
+    obj_mr = obj['mr']
+    verb_rt = verb['root']
     verb_mr = inflect_verb(verb_rt , person , plural, gender , tense)
 
     return f"{sub_mr} {obj_mr} {verb_mr}"
